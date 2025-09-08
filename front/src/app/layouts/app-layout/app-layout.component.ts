81d8da50f56673b8e21ce17420741c0cf0df388d
LHOTTE Lucien <lucien.lhotte@orange.com>
2025-09-08 14:42:49 +0200

    mobile navbar

--- front/src/app/layouts/app-layout/app-layout.component.ts
+++ front/src/app/layouts/app-layout/app-layout.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { SessionService } from 'src/app/services/session.service';
 
 @Component({
-  selector: 'app-app-layout',
+  selector: 'app-auth-layout',
   templateUrl: './app-layout.component.html',
   styleUrls: ['./app-layout.component.scss'],
 })
 export class AppLayoutComponent {
+  menuOpen = false;
+  innerWidth = window.innerWidth;
+
   constructor(private sessionService: SessionService, private router: Router) {}
+
+  @HostListener('window:resize')
+  onResize() {
+    this.innerWidth = window.innerWidth;
+    if (this.innerWidth > 768) this.menuOpen = false;
+  }
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
 
   logout(): void {
     this.sessionService.logOut();
-    this.router.navigate(['/']);
+    this.router.navigate(['']);
+    this.menuOpen = false;
   }
 }
